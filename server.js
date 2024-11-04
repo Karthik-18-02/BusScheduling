dd695590c8afdb5b0fa234eaf26a5b20a9644986
Karthik-18-02 <karthiksamala33@gmail.com>
2024-11-04 13:30:30 +0530

    duplicate

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/mini", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost:27017/mini", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
