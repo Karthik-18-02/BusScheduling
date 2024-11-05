6496075f5e7fe985e40c3fbf7ee02e1501f2546d
Karthik-18-02 <karthiksamala33@gmail.com>
2024-11-05 11:03:56 +0530

    com

--- server.js
+++ server.js
@@ -73,9 +73,12 @@ app.use(express.static("public"));
 //   useUnifiedTopology: true,
 // });
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("MongoDB connection error:", error));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((error) => console.error("MongoDB connection error:", error));
 
 // Define user schema (including admin roles)
 const UserSchema = new mongoose.Schema({
