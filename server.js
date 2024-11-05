72d8cbc5c65fb06dd567843e54a821364788803e
Karthik-18-02 <karthiksamala33@gmail.com>
2024-11-05 12:03:42 +0530

    m

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.use(express.static("public"));
 //   useUnifiedTopology: true,
 // });
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI) //
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("MongoDB connection error:", error));
 
